b8f90b3cf62bd25efd8185460e4f450163d0f1ed
maxmay94 <maxmay94@gmail.com>
2022-04-11 18:43:59 -0400

    click button to get new image

--- src/pages/Reviews/reviews.jsx
+++ src/pages/Reviews/reviews.jsx
@@ -6,24 +6,31 @@ let rand = Math.round(Math.random() * 856655)
 
 const Reviews = ({user}) => {
   const [art, setArt] = useState({})
-
+  
   useEffect(() => {
     getArtData(rand)
   }, [])
 
+  useEffect(() => {
+    console.log('useEffect() art ---> ', art)
+  }, [art])
+
   function getArtData(rand) {
-    let art = `${API_URL + rand}`
-    fetch(art).then((res) => res.json()).then(data => {
-      if(!data.primaryImage) getArtData(Math.round(Math.random() * 856655))
-      else setArt(data)
-    })
-    .catch(err => console.log(err))
+    fetch(`${API_URL + rand}`)
+      .then((res) => res.json())
+      .then(data => {
+        if(!data.primaryImage) getArtData(Math.round(Math.random() * 856655))
+        else setArt(data)
+      })
+    .catch(err => console.log('ERROR',err))
   }
 
-  
   return (
     <div className="flex-auto text-center min-h-screen">
-      <h1 className="rounded-sm m-20 font-semibold text-6xl text-white bg-red-800/[.8]"> <Link to=''>review a random work of art</Link> </h1>
+      <h1 className="rounded-sm m-20 font-semibold text-6xl text-white bg-red-800/[.8]"> <button onClick={() => {getArtData(rand)}} >view a random work of art</button> </h1>
+      <div className='flex m-10 p-10 bg-black/[.9]'>
+        <img className='max-h-max max-w-full self-center' src={art.primaryImage} alt="" />
+      </div>
     </div>
   )
 }
