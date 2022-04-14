6c4d7065958531dc55600f8c684c27f49f8ba513
maxmay94 <maxmay94@gmail.com>
2022-04-13 20:20:16 -0400

    optomize landing page for mobile

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -2,11 +2,35 @@ import { Link } from 'react-router-dom'
 
 const Landing = ({ user }) => {
   return (
-    <main className="flex-auto text-center text-amber-500  min-h-screen">
+    <main className="flex-auto text-center min-h-screen">
       <div>
-        <h1 className='m-20 font-semibold rounded-sm border-0 bg-amber-500/[.7] text-black/[.8] hover:text-white/[.8] text-9xl'>art critic.</h1>
-        <h1 className='m-20 font-semibold rounded-sm border-0 bg-pink-600/[.45] hover:bg-pink-600/[.7] text-white/[.8] hover:text-blue-800/[.8]  text-7xl'>{user && `hey, ${user.name}.`}</h1> 
-        <Link to='/reviews/new' ><h1 className='m-20 font-semibold rounded-sm border-0 bg-orange-600/[.45] hover:bg-orange-600/[.7] text-white/[.8] hover:text-rose-900/[.8]  text-5xl'>{user && 'let\'s look at some art.'}</h1></Link>
+
+        <h1 className='m-5 sm:m-10 md:m-10 lg:m-20 
+            font-semibold rounded-sm border-0 
+            bg-amber-500/[.7] text-black/[.8] hover:text-white/[.8] 
+            text-7xl sm:text-8xl md:text-9xl'
+        >
+          art critic.</h1>
+
+
+        <h1 className='m-5 sm:m-10 md:m-10 lg:m-20 
+            font-semibold rounded-sm border-0 
+            bg-pink-600/[.45] hover:bg-pink-600/[.7] 
+            text-white/[.8] hover:text-blue-800/[.8]  
+            text-5xl sm:text-7xl'
+        >
+          {user && `hey, ${user.name}.`}</h1> 
+
+
+        <Link to='/reviews/new' >
+          <h1 className='m-5 sm:m-10 md:m-10 lg:m-20 
+              font-semibold rounded-sm border-0 
+              bg-orange-600/[.45] hover:bg-orange-600/[.7] 
+              text-white/[.8] hover:text-rose-900/[.8] 
+              text-3xl sm:text-5xl'
+          >
+          {user && 'let\'s look at some art.'}</h1>
+        </Link>
       </div>
     </main>
   )
