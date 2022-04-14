bdda6f6fa9b4163e3a053081a374356685650186
maxmay94 <maxmay94@gmail.com>
2022-04-13 21:04:58 -0400

    slight style change

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,10 +5,10 @@ const Landing = ({ user }) => {
     <main className="flex-auto text-center min-h-screen">
       <div>
 
-        <h1 className='m-5 sm:m-10 md:m-10 lg:m-20 
+        <h1 className='m-5 mt-20 sm:m-10 sm:mt-20 md:m-10 md:mt-20 lg:m-20 
             font-semibold rounded-sm border-0 
             bg-amber-500/[.7] text-black/[.8] hover:text-white/[.8] 
-            text-7xl sm:text-8xl md:text-9xl'
+            text-8xl md:text-9xl'
         >
           art critic.</h1>
 
