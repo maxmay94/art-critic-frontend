7c89c7fce2e7b96ba0bb56a7d1c3308f122e9edd
maxmay94 <maxmay94@gmail.com>
2022-04-14 16:47:54 -0400

    update responivness for 2xl screens on index

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,10 +7,10 @@ const NavBar = ({ user, handleLogout }) => {
         {user ?
           <nav>
             <ul className='flex flex-row'>
-              <li className='grow m-2'><Link className='bg-black/[.7] text-white/[.8] hover:text-amber-200/[.8] font-semibold p-2 rounded-sm' to='/'>{user.name.toLowerCase()}</Link></li>
-              <li className='flex-end m-2'><Link className='hover:text-black/[.8] bg-amber-500/[.3] hover:bg-yellow-600/[.6] font-semibold p-2 rounded-sm' to="/reviews/new">write a review</Link></li>
-              <li className='flex-end m-2'><Link className='hover:text-blue-800/[.8] bg-pink-700/[.3] hover:bg-pink-600/[.6] font-semibold p-2 rounded-sm' to="/reviews/index">reviews</Link></li>
-              <li className='m-2'><Link className='hover:text-rose-900/[.8] bg-orange-600/[.3] hover:bg-orange-600/[.6] font-semibold p-2 rounded-sm' to="" onClick={handleLogout}>log out</Link></li>
+              <li className='grow m-2'><Link className='bg-black/[.2] hover:bg-black/[.6] text-white/[.8] hover:text-amber-200/[.8] font-semibold p-2 rounded-sm' to='/'>{user.name.toLowerCase()}</Link></li>
+              <li className='flex-end m-2'><Link className='hover:text-black/[.8] bg-amber-500/[.2] hover:bg-yellow-600/[.6] font-semibold p-2 rounded-sm' to="/reviews/new">write a review</Link></li>
+              <li className='flex-end m-2'><Link className='hover:text-blue-800/[.8] bg-pink-700/[.2] hover:bg-pink-600/[.6] font-semibold p-2 rounded-sm' to="/reviews/index">reviews</Link></li>
+              <li className='m-2'><Link className='hover:text-rose-900/[.8] bg-orange-600/[.2] hover:bg-orange-600/[.6] font-semibold p-2 rounded-sm' to="" onClick={handleLogout}>log out</Link></li>
             </ul>
           </nav>
         :
--- src/pages/Reviews/ReviewIndex.jsx
+++ src/pages/Reviews/ReviewIndex.jsx
@@ -5,7 +5,7 @@ const ReviewIndex = ({user, reviews, deleteReview, editReview}) => {
     <div className="min-h-screen">
       <h1 className='m-5 lg:mx-20 lg:my-5 font-semibold rounded-sm border-0 bg-amber-500/[.7] text-black/[.8] hover:text-white/[.8] text-6xl'> judgement. </h1>
       <div className="bg-black/[.6] p-5 mx-5 lg:mx-20 min-h-full rounded max-h-screen overflow-scroll">
-        <div className="m-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">
+        <div className="m-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 xl:gap-x-8">
           {reviews ?
             reviews.map((review, i) => (
               review.art_id &&
