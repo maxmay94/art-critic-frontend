cdf25d4cae187b85317b277d0424749075d05572
maxmay94 <maxmay94@gmail.com>
2022-04-14 16:17:36 -0400

    update nav style

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,21 +3,21 @@ import { Link } from 'react-router-dom'
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
-      <div className='bg-gray-600/[.7] hover:bg-gray-600/[.9] text-slate-200 font-sans font-semibold  h-10'>
+      <div className='bg-gray-600/[.2] hover:bg-gray-600/[.4] text-slate-200 font-sans font-semibold  h-10'>
         {user ?
           <nav>
             <ul className='flex flex-row'>
-              <li className='grow m-2'><Link className='font-bold hover:text-orange-500' to='/'>{user.name.toLowerCase()}</Link></li>
-              <li className='grow m-2'><Link className='hover:text-orange-500 bg-gray-700/[.8] p-2 rounded-sm' to="/reviews/new">write a review</Link></li>
-              <li className='grow m-2'><Link className='hover:text-orange-500 bg-gray-700/[.8] p-2 rounded-sm' to="/reviews/index">reviews</Link></li>
-              <li className='m-2'><Link className='hover:text-orange-500 bg-gray-700/[.8] p-2 rounded-sm' to="" onClick={handleLogout}>log out</Link></li>
+              <li className='grow m-2'><Link className='font-bold bg-amber-500/[.7] text-black/[.8] p-2 rounded-sm' to='/'>{user.name.toLowerCase()}</Link></li>
+              <li className='flex-end m-2'><Link className='hover:text-black/[.8]  bg-yellow-700/[.3] hover:bg-yellow-700/[.5] p-2 rounded-sm' to="/reviews/new">write a review</Link></li>
+              <li className='flex-end m-2'><Link className='hover:text-black/[.8]  bg-pink-700/[.3] hover:bg-pink-700/[.5] p-2 rounded-sm' to="/reviews/index">reviews</Link></li>
+              <li className='m-2'><Link className='hover:text-black/[.8]  bg-orange-700/[.3] hover:bg-orange-700/[.5] p-2 rounded-sm' to="" onClick={handleLogout}>log out</Link></li>
             </ul>
           </nav>
         :
           <nav>
             <ul className='flex flex-row'>
-              <li className='m-2 hover:text-black'><Link className='hover:text-orange-500 bg-gray-700/[.8] p-2 rounded-sm' to="/login">log in.</Link></li>
-              <li className='m-2 hover:text-black'><Link className='hover:text-orange-500 bg-gray-700/[.8] p-2 rounded-sm' to="/signup">sign up.</Link></li>
+              <li className='m-2 hover:text-black'><Link className='hover:text-black/[.8]  bg-gray-700/[.8] p-2 rounded-sm' to="/login">log in.</Link></li>
+              <li className='m-2 hover:text-black'><Link className='hover:text-black/[.8]  bg-gray-700/[.8] p-2 rounded-sm' to="/signup">sign up.</Link></li>
             </ul>
           </nav>
         }
